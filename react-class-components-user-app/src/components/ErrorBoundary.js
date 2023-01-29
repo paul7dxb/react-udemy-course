685fd518e36ba4cf2121b8182b15a19e39cb1220
paul7dxb <62883464+paul7dxb@users.noreply.github.com>
2023-01-29 20:21:12 +0000

    End Section 13

---
+++ react-class-components-user-app/src/components/ErrorBoundary.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class ErroryBoundary extends Component {
+  constructor() {
+    super();
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error) {
+    //error check logic
+    console.log(error);
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p> Something Went Wrong! </p>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErroryBoundary;
